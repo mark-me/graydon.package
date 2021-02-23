36ae6354738d98fa81d445d10020143850fffe44
jessicav <jessicav@graydon-global.com>
2021-02-23 15:48:45 +0100

    delete

--- R/sp_de_deelstaten.R
+++
@@ -1,7 +0,0 @@
-#' Map outlining the German 'deelstaten'
-#'
-#' Germany has 16 deelstaten or Bundesländer. Read more on these administrative areas in Germany on \href{https://nl.wikipedia.org/wiki/Deelstaten_van_Duitsland}{Wikipedia}.
-#'
-#' @format An object of class SpatialPolygonsDataFrame with 16 rows and 4 columns.
-#' @source Shapefile data online available \href{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}
-"sp_de_deelstaten"
