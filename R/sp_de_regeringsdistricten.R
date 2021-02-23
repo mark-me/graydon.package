6f7d0f29676ed37b4e01222dd8f019b490f933c0
jessicav <jessicav@graydon-global.com>
2021-02-23 15:29:44 +0100

    Add German map data on level regeringsdistricten

---
+++ R/sp_de_regeringsdistricten.R
@@ -0,0 +1,7 @@
+#' Map outlining the German 'regeringsdistricten'
+#'
+#' Germany has 19 'regeringsdistricten' or 'Regierungsbezirk'. These are areas in Germany that contain multiple districts or Kreise. Read more on these administrative areas in Germany on \href{https://nl.wikipedia.org/wiki/Regierungsbezirk}{Wikipedia}.
+#'
+#' @format An object of class SpatialPolygonsDataFrame with 401 rows and 4 columns.
+#' @source Shapefile data online available \href{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}
+"sp_de_regeringsdistricten"
