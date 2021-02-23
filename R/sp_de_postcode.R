e00c4e79c094b9c78cad78f1d7af0a99f0d923ed
jessicav <jessicav@graydon-global.com>
2021-02-23 15:25:51 +0100

    Add German map data on zip code

---
+++ R/sp_de_postcode.R
@@ -0,0 +1,7 @@
+#' Map outlining the German postal code borders
+#'
+#' Read more on postal codes in Germany on \href{https://en.wikipedia.org/wiki/List_of_postal_codes_in_Germany}{Wikipedia}.
+#'
+#' @format An object of class SpatialPolygonsDataFrame with 8725 rows and 2 columns.
+#' @source Shapefile data online available \href{https://www.suche-postleitzahl.org/downloads}{https://www.suche-postleitzahl.org/downloads}
+"sp_de_postcode"
