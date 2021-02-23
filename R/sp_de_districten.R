cc75b92b0496803624bea014fa4ec8ed2d608989
jessicav <jessicav@graydon-global.com>
2021-02-23 15:28:18 +0100

    Add German map data on district (Kreise) level

---
+++ R/sp_de_districten.R
@@ -0,0 +1,7 @@
+#' Map outlining the German districts
+#'
+#' Germany has 401 districts or 'Kreise'. Read more on these administrative areas in Germany on \href{https://nl.wikipedia.org/wiki/Landkreis}{Wikipedia}.
+#'
+#' @format An object of class SpatialPolygonsDataFrame with 401 rows and 4 columns.
+#' @source Shapefile data online available \href{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}{https://gdz.bkg.bund.de/index.php/default/digitale-geodaten/verwaltungsgebiete/verwaltungsgebiete-1-2-500-000-stand-01-01-vg2500.html}
+"sp_de_districten"
